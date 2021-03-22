da19afab0991a2afc0b09ddb9e5e94fb2278ba2b
Salumao Barobsa da Costa <salubcosta@gmail.com>
2021-03-22 11:43:13 -0300

    Testando queries de selecao com clausulas where e recursos como all, find, first, get e whereId

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,24 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/queries', function () {
+    $events = \App\Models\Event::all(); # select * from events;
+    // $events = \App\Models\Event::all(['title', 'description']); # select title, description from events;
+    
+    /** 
+     * get()        -> retorna uma colection
+     * first()      -> retorna dado único
+     * find($param) -> retorna dado único
+     * 
+     * $events = \App\Models\Event::whereId(1)->get(); # select * from events where id = 1;
+     * $events = \App\Models\Event::where('id',1)->get(); # select * from events where id = 1;
+     * $events = \App\Models\Event::where('id', 1)->first(); # select * from events where id = 1;
+     * $events = \App\Models\Event::find(1); # select * from events where id = 1;
+     */
+    
+    return $events;
+});
+
 Route::get('/hello-world', [\App\Http\Controllers\HelloWorldController::class, 'helloWorld']);
 
 Route::get('/hello/{name?}', [\App\Http\Controllers\HelloWorldController::class, 'hello']);
